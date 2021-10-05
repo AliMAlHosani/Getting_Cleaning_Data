25479ba917ff682e9876392de8890f50f9ec69d0
AliMAlHosani <42833576+AliMAlHosani@users.noreply.github.com>
2021-10-05 12:19:47 +0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,24 @@
 library(dplyr)
+
+#Reading files
 features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
 activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+
+#Test tables
 subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
 x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
 y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+
+#Train tables
 subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
 x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
+
 Subject <- rbind(subject_train, subject_test)
+
 Merged_Data <- cbind(Subject, Y, X)
 TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
 TidyData$code <- activities[TidyData$code, 2]
